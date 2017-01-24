a97c5f9b6c182c4892154f8d38a32de524a8d63e
laurent vanni <laurent.vanni@gmail.com>
2017-01-24 15:24:32 +0100

    UP: predict

--- predict.py
+++ predict.py
@@ -6,16 +6,11 @@ Created on 7 déc. 2016
 @author: lvanni
 '''
 from contextlib import closing
-import os
 import pickle
 import sys
-
-import numpy
 import theano
 
-from core.config import EMBEDDING_DICO, DWIN, VECT_SIZE, N_HIDDEN, NLP_PATH, \
-    CORPUS_PATH, CORPUS_TYPE
-from core.preprocess.dico import get_input_from_files
+from core.config import EMBEDDING_DICO, DWIN, VECT_SIZE, N_HIDDEN, NLP_PATH
 from core.training.lookup import LookUpTrain
 from core.training.main import pre_process
 import theano.tensor as T
@@ -23,10 +18,10 @@ import theano.tensor as T
 
 if __name__ == '__main__':
     
-    text_to_test = sys.argv[1]
+    text_to_test = {}
+    text_to_test["text"] = [sys.argv[1]] 
     
     print "Chargement de l'embedding.................",
-    x_value = []
     try:
         with closing(open(EMBEDDING_DICO, 'rb')) as f:
             dico = pickle.load(f)
@@ -42,31 +37,12 @@ if __name__ == '__main__':
     n_mot = [len(dico[i]) for i in dico.keys()]
     print  "OK"
     
-    # Compte nb classes
-    corpus = []
-    nb_class = 0
-    for filename in os.listdir(CORPUS_PATH):
-        filename_args = filename.split(".")
-        try:
-            if filename_args[-2] == CORPUS_SIZE and filename_args[-1] == CORPUS_TYPE:
-                #corpus.append(filename_args[0])
-                corpus.append(CORPUS_PATH + filename)
-                nb_class += 1
-        except:
-            continue
-        
     # Natural Langage Processing
-    t_nlp = LookUpTrain(DWIN, n_mot, VECT_SIZE, N_HIDDEN, n_out=nb_class)
+    t_nlp = LookUpTrain(DWIN, n_mot, VECT_SIZE, N_HIDDEN)
     t_nlp.load(NLP_PATH, "network_state_0")
     
-    print "Chargement du texte.......................",    
-    lines, _ = get_input_from_files([text_to_test], dico)
-    for line in lines:
-        x_value.append(line)
-    print  "OK"
-    
     # Preprocessing => découpage du texte
-    x_train, x_valid, x_test, tmp, tmp, tmp = pre_process([text_to_test])
+    x_train, x_valid, x_test, tmp, tmp, tmp = pre_process(text_to_test)
 
     # concatener des arrays numpy
     #x_cont = numpy.concatenate([x_train, x_valid, x_test], axis=0)
@@ -78,6 +54,7 @@ if __name__ == '__main__':
     probabilities = theano.function(inputs=[x], outputs=t_nlp.probabilities_text(x), allow_input_downcast=True)
     # A TESTER
     proba_list = probabilities(x_test)
+    print proba_list
     
     # sinon
     # np.mean([probabilities(x_cont[index*batch_size:(index+1)*batch_size])])
@@ -87,8 +64,10 @@ if __name__ == '__main__':
     
     # test predict
     # Fonction de prediction : Pour une phrase donnée, quel est le président
+    """
     predict = theano.function(inputs=[x], outputs=t_nlp.predict(x), allow_input_downcast=True)
     for test in x_test:
         print "predict :", corpus[predict([test])].replace(CORPUS_PATH, "").split(".")[0]
+    """
     
     
