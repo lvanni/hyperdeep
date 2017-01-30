dcb4dd23ffa4f3fc1074cb08339f75ca7d2412e0
laurent vanni <laurent.vanni@gmail.com>
2017-01-30 16:30:11 +0100

    REFACTORING: setup.py works now

---
+++ deeperbase/core/config.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*-coding:Utf-8 -*
+'''
+Created on 7 déc. 2016
+
+@author: lvanni
+'''
+
+# Back prop
+LEARNING_RATE = 1e-2
+DECACY_RATE   = 0.4
+BATCH_SIZE    = 32
+
+DWIN = 20                     # Nb de mots dans une phrase
+VECT_SIZE  = [100, 10, 5, 5]  # Nb de feature associées à chaque niveau (forme/lemme/code/fonction)
+N_HIDDEN   = [100, 50]        # 2 couches : 100 neurones + 50 neurones
+
+# DEFAULT PATH
+DATA_PATH   = "../data/"
+CORPUS_PATH = DATA_PATH + "corpus/"
+NLP_PATH    = DATA_PATH + "nlp/"
+DICO_PATH   = DATA_PATH + "dico/"
+
+NLP             = "network_state"
+EMBEDDING_DICO  = "embedding_dico.txt"
+OCC_DICO        = "occ_dico.txt"
+
